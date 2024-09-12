2a7d92f6aa8145366577a17e291fa49d9ae8af4d
Ndbeloved <ndbeloved001@gmail.com>
2024-09-12 09:12:39 +0100

    feat/building navigation carousel

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,17 +1,52 @@
-import { FaSearch } from "react-icons/fa"
+import { FaRegBuilding, FaSearch } from "react-icons/fa"
 import logo from "/logo-splash-512x512.png"
+import { PiBuildingApartment, PiBuildingOfficeBold } from "react-icons/pi"
 import "./Navigation.css"
+import {  TbBuilding, TbBuildingBroadcastTowerFilled, TbBuildingCottage, TbBuildingEstate, TbBuildingSkyscraper } from "react-icons/tb"
+import { LuBed } from "react-icons/lu"
+import { IoHomeOutline } from "react-icons/io5"
 
 const Navigation = () => {
     return (
         <nav className="navigation-bar">
             <div className="row-1">
-                <img src={logo} />
-            </div>
-            <div className="row-2">
                 <div className="search-bar">
                     <FaSearch />
                     <p>Search for Accomodation</p>
+                </div>
+            </div>
+            <div className="row-2">
+                <div className="item">
+                    <FaRegBuilding />
+                    Lodges
+                </div>
+                <div className="item">
+                    <LuBed />
+                    Self-contained
+                </div>
+                <div className="item">
+                    <TbBuilding />
+                    1-Bedroom
+                </div>
+                <div className="item">
+                    <TbBuildingEstate />
+                    2-Bedroom
+                </div>
+                <div className="item">
+                    <TbBuildingCottage />
+                    3-Bedroom
+                </div>
+                <div className="item">
+                    <TbBuildingSkyscraper />
+                    Duplex
+                </div>
+                <div className="item">
+                    <PiBuildingApartment />
+                    Boys' Quarters
+                </div>
+                <div className="item">
+                    <PiBuildingOfficeBold />
+                    Office
                 </div>
             </div>
         </nav>

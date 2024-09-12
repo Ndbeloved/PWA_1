667e9e41c87b119c98c97f95d9608087640849ab
Ndbeloved <ndbeloved001@gmail.com>
2024-09-12 03:40:21 +0100

    fix/occupying fullscreen

--- src/Components/Modal/BottomNavigation/BottomNavigation.jsx
+++ src/Components/Modal/BottomNavigation/BottomNavigation.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import IconButtonWithCaption from "../../IconButtonWithCaption/IconButtonWithCaption"
 import "./BottomNavigation.css"
-import HomeIcon from '@mui/icons-material/HomeRounded';
+import HomeIcon from '@mui/icons-material/HomeRounded'
 import UserIcon from "@mui/icons-material/PersonRounded"
 import DownloadIcon from "@mui/icons-material/DownloadRounded"
 import ArticleIcon from "@mui/icons-material/ArticleRounded"
+import SearchOutlined from "@mui/icons-material/SearchOutlined"
 
 const BottomNavigation = () => {
     const [activeEl, setActiveEl] = useState(0)
@@ -15,9 +16,10 @@ const BottomNavigation = () => {
 
     return (
         <footer className='bottom-navigation'>
-            <IconButtonWithCaption isActive={activeEl === 0} label="Home" Icon={HomeIcon} onClick={()=>{setActiveEl(0)}} />
-            <IconButtonWithCaption isActive={activeEl === 3} label="Blogs" Icon={ArticleIcon} onClick={()=>{setActiveEl(3)}} />
-            <IconButtonWithCaption isActive={activeEl === 1} label="Downloads" Icon={DownloadIcon} onClick={()=>{setActiveEl(1)}}/>
+            <IconButtonWithCaption isActive={activeEl === 0} label="For you" Icon={HomeIcon} onClick={()=>{setActiveEl(0)}} />
+            <IconButtonWithCaption isActive={activeEl === 3} label="Explore" Icon={SearchOutlined} onClick={()=>{setActiveEl(3)}} />
+            <IconButtonWithCaption isActive={activeEl === 4} label="Alerts" Icon={ArticleIcon} onClick={()=>{setActiveEl(4)}} />
+            <IconButtonWithCaption isActive={activeEl === 1} label="Saved" Icon={DownloadIcon} onClick={()=>{setActiveEl(1)}}/>
             <IconButtonWithCaption isActive={activeEl === 2} label="Profile" Icon={UserIcon} onClick={()=>{setActiveEl(2)}} />
         </footer>
     )
